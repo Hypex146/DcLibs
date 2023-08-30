52a7a90e6b4708e9b9f571f9f66e5dd93a558868
Kolomenskiy_Vladislav <vlad-go146@yandex.ru>
2023-08-30 20:00:51 +0300

    Add javadoc for class "DcBlockUtilities"

--- src/utilities/DcBlockUtilities.java
+++ src/utilities/DcBlockUtilities.java
@@ -14,8 +14,20 @@ import org.bukkit.util.BlockIterator;
 public abstract class DcBlockUtilities {
 	
 	
-	// Collection<Block> getBlocks
-	// 1. limit + filter
+	/**
+	 * <p>
+	 * Get blocks intersected by a given vector
+	 * </p><p>
+	 * Note: 1. limit, filter
+	 * </p>
+	 * @param iterator - Iterator defining the block selection vector
+	 * @param start_loc - The position of the beginning of the vector
+	 * @param max_dist - Maximum block search distance
+	 * @param limit - Maximum number of selected blocks
+	 * @param filter - Block selection condition
+	 * @return A collection of blocks, maybe empty
+	 * @since 0.0.1
+	 */
 	public static Collection<Block> getBlocks(BlockIterator iterator, 
 			Location start_loc, Double max_dist, Integer limit, 
 			Predicate<Material> filter) {
@@ -41,27 +53,53 @@ public abstract class DcBlockUtilities {
 		return blocks;
 	}
 	
-	// 2. limit
+	/**
+	 * <p>
+	 * Note: 2. limit
+	 * </p>
+	 * @see #getBlocks(BlockIterator, Location, Double, Integer, Predicate)
+	 */
 	public static Collection<Block> getBlocks(BlockIterator iterator, 
 			Location start_loc, Double max_dist, Integer limit) {
 		return getBlocks(iterator, start_loc, max_dist, limit, null);
 	}
 	
-	// 3. filter
+	/**
+	 * <p>
+	 * Note: 3. filter
+	 * </p>
+	 * @see #getBlocks(BlockIterator, Location, Double, Integer, Predicate)
+	 */
 	public static Collection<Block> getBlocks(BlockIterator iterator, 
 			Location start_loc, Double max_dist, Predicate<Material> filter) {
 		return getBlocks(iterator, start_loc, max_dist, -1, filter);
 	}
 	
-	// 4.
+	/**
+	 * <p>
+	 * Note: 4.
+	 * </p>
+	 * @see #getBlocks(BlockIterator, Location, Double, Integer, Predicate)
+	 */
 	public static Collection<Block> getBlocks(BlockIterator iterator, 
 			Location start_loc, Double max_dist) {
 		return getBlocks(iterator, start_loc, max_dist, -1, null);
 	}
 	
 	
-	// Block getBlock
-	// 1. filter
+	/**
+	 * <p>
+	 * Get the first block intersecting with a given vector
+	 * </p><p>
+	 * Note: 1. filter
+	 * <p>
+	 * @param iterator - Iterator defining the block selection vector
+	 * @param start_loc - The position of the beginning of the vector
+	 * @param max_dist - Maximum block search distance
+	 * @param filter - Block selection condition
+	 * @return intersected block, maybe null
+	 * @since 0.0.1
+	 */
 	public static Block getBlock(BlockIterator iterator, 
 			Location start_loc, Double max_dist, 
 			Predicate<Material> filter) {
@@ -74,15 +112,32 @@ public abstract class DcBlockUtilities {
 		return iterator_block.next();
 	}
 	
-	// 2.
+	/**
+	 * <p>
+	 * Note: 2.
+	 * </p>
+	 * @see #getBlock(BlockIterator, Location, Double, Predicate)
+	 */
 	public static Block getBlock(BlockIterator iterator, 
 			Location start_loc, Double max_dist) {
 		return getBlock(iterator, start_loc, max_dist, null);
 	}
 	
 	
-	// Boolean hasBlock
-	// 1. filter
+	/**
+	 * <p>
+	 * Does the vector intersect any block?
+	 * </p><p>
+	 * Note: 1. filter
+	 * </p>
+	 * @param iterator - Iterator defining the block selection vector
+	 * @param start_loc - The position of the beginning of the vector
+	 * @param max_dist - Maximum block search distance
+	 * @param filter - Block selection condition
+	 * @return True - if the vector intersects the block, considering 
+	 * the filter, otherwise false
+	 * @since 0.0.1
+	 */
 	public static Boolean hasBlock(BlockIterator iterator, 
 			Location start_loc, Double max_dist, 
 			Predicate<Material> filter) {
@@ -95,7 +150,12 @@ public abstract class DcBlockUtilities {
 		}
 	}
 	
-	// 2.
+	/**
+	 * <p>
+	 * Note: 2.
+	 * </p>
+	 * @see #hasBlock(BlockIterator, Location, Double, Predicate)
+	 */
 	public static Boolean hasBlock(BlockIterator iterator, 
 			Location start_loc, Double max_dist) {
 		return hasBlock(iterator, start_loc, max_dist, null);
